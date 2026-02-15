3afde78760ab5b11aef96111129fba582b7fdbfe
chilgoe <zonda037@gmail.com>
2026-02-15 15:09:01 +0700

    repo: lab 2 add upload file feature

--- lab2/index.ts
+++ lab2/index.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response } from 'express';
+import path from 'path';
 import teacherRouter from './routes/teacher';
+import uploadRouter from './routes/upload';
 import { initTables } from './connection/init';
 
 const app = express();
 const port = 3000;
 
+
 app.use(express.json());
+
+app.use('/public', express.static(path.join(process.cwd(), 'public')));
+
 app.use('/teacher', teacherRouter);
+app.use('/upload', uploadRouter);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello! Mr.Goe');
